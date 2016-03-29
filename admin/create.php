09c5df8680bb8aa9925ada1f188ed0acf6bc4568
James Droste <james@droste.im>
2016-03-29 00:04:03 -0400

    Create done; edit changes to make it look nicer

--- admin/create.php
+++ admin/create.php
@@ -10,48 +10,33 @@ include '../includes/session.php';
 requireLogin();
 requireAdmin();
 
-if ( !isset($_GET['id']) || !is_numeric($_GET['id']) ) {
-    header('Location: /admin/people.php');
-    die;
-}
-
-$stmt = $db->prepare('SELECT * FROM users WHERE id = ?');
-$stmt->execute(array($_GET['id']));
-
-if ( $stmt->rowCount() != 1 ) {
-    header('Location: /admin/people.php');
-    die;
-}
-
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
-
 if ( !empty($_POST) ) {
-    $query = 'UPDATE users SET %s WHERE id = ?';
+    $query = 'INSERT INTO users (%s) VALUES(%s)';
 
+    $keys   = array();
     $params = array();
     $values = array();
 
     foreach ( $_POST AS $key => $val ) {
         if ( empty($val) ) continue;
-        if ( $user[$key] == $val ) continue;
 
         if ( $key == 'password' ) $val = sha1($val);
 
-        $params[] = $key.' = ?';
+        $keys[] = $key;
+        $params[] = '?';
         $values[] = $val;
     }
 
     if ( !empty($values) ) {
-        $values[] = $_GET['id'];
-
-        $query = sprintf($query, implode(',', $params));
+        $query = sprintf($query, implode(',', $keys), implode(',', $params));
         $stmt = $db->prepare($query);
         $stmt->execute($values);
 
         if ( $stmt->rowCount() == true ) {
-            $user = $_POST;
+            header('Location: /admin/people.php');
+            die;
         } else {
-            die('Failed to update the DB');
+            die('DB insert error');
         }
     }
 }
@@ -67,15 +52,11 @@ include '../includes/header.php';
 
     <hr>
 
-    <?php if ( !empty($_POST) ): ?>
-    <div class="alert alert-success">Updated user!</div>
-    <?php endif; ?>
-
     <form class="form-horizontal" method="post">
         <div class="form-group">
             <label for="username" class="col-sm-2 control-label">Username</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control" name="username" id="username" value="<?php echo $user['username']; ?>">
+                <input type="text" class="form-control" name="username" id="username" value="">
             </div>
         </div>
 
@@ -89,41 +70,41 @@ include '../includes/header.php';
         <div class="form-group">
             <label for="admin" class="col-sm-2 control-label">Is Admin (1 = Is Admin)</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control" name="admin" id="admin" value="<?php echo $user['admin']; ?>">
+                <input type="text" class="form-control" name="admin" id="admin" value="">
             </div>
         </div>
 
         <div class="form-group">
             <label for="ssn" class="col-sm-2 control-label">SSN</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control" name="ssn" id="ssn" value="<?php echo $user['ssn']; ?>">
+                <input type="text" class="form-control" name="ssn" id="ssn" value="">
             </div>
         </div>
 
         <div class="form-group">
             <label for="email" class="col-sm-2 control-label">Email</label>
             <div class="col-sm-10">
-                <input type="email" class="form-control" name="email" id="email" value="<?php echo $user['email']; ?>">
+                <input type="email" class="form-control" name="email" id="email" value="">
             </div>
         </div>
 
         <div class="form-group">
             <label for="phone" class="col-sm-2 control-label">Phone</label>
             <div class="col-sm-10">
-                <input type="text" class="form-control" name="phone" id="phone" value="<?php echo $user['phone']; ?>">
+                <input type="text" class="form-control" name="phone" id="phone" value="">
             </div>
         </div>
 
         <div class="form-group">
             <label for="address" class="col-sm-2 control-label">Address</label>
             <div class="col-sm-10">
-                <textarea class="form-control" name="address" id="address" rows="3"><?php echo $user['address']; ?></textarea>
+                <textarea class="form-control" name="address" id="address" rows="3"></textarea>
             </div>
         </div>
 
         <div class="form-group">
             <div class="col-sm-offset-2 col-sm-10">
-                <button type="submit" class="btn btn-default">Update</button>
+                <button type="submit" class="btn btn-default">Create</button>
             </div>
         </div>
     </form>
